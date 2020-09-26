be4912edbc603d0410315295597a6a48bfe8bf36
julietvt <freshitpt@gmail.com>
2020-09-26 12:28:40 +0300

    email valid schema

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose');
 const db = mongoose.connection;
+mongoose.connect('mongodb://localhost:27017/pd2020_dbtest',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}, function (err) {
+    if(err){console.log("Error connection");}
+    else{console.log("Success connection!");}
+});
+
+const yup = require('yup');
+const emailValidSchema = yup.string().email();
+
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
     firstName: {
@@ -19,12 +30,10 @@ const userSchema = new Schema({
         type: String,
         required: true,
         unique: true,
-    },
-});
-mongoose.connect('mongodb://localhost:27017/pd2020_dbtest',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}, function (err) {
-    if(err){console.log("Error connection");}
-    else{console.log("Success connection!");}
-});
+        validate: {
+            validator: value => emailValidSchema.validate(value);
+        }
+    }
+    });
+
+
