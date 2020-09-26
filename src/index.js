e1d6ea4fb41b78d11a6533b4aee24e01f7541375
julietvt <freshitpt@gmail.com>
2020-09-26 13:15:36 +0300

    create stucture project

--- src/index.js
+++ src/index.js
@@ -1,52 +1,15 @@
-const mongoose = require('mongoose');
-const db = mongoose.connection;
-mongoose.connect('mongodb://localhost:27017/pd2020_dbtest',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}, function (err) {
-    if(err){console.log("Error connection");}
-    else{console.log("Success connection!");}
-});
-
-const yup = require('yup');
-const emailValidSchema = yup.string().email();
-
-const Schema = mongoose.Schema;
-const userSchema = new Schema({
-    firstName: {
-        type: String,
-        minLength: 1,
-    },
-    lastName: {
-        type: String,
-        minLength: 1,
-    },
-    role: {
-        type: String,
-        enum: ['USER', 'MODERATOR', 'ADMIN'],
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        validate: {
-            validator: value => emailValidSchema.validate(value),
-        }
-    }
-    });
-
-const User = mongoose.model('User',userSchema);
+const db = require('./db');
+const User = require('./models/user.model');
 
 function insertUser(body) {
-    const user = new User(body);
-    user.save().then(console.log).catch(console.error);
+    const user_obj = new User(body);
+    user_obj.save().then(console.log).catch(console.error);
 }
 
 insertUser({
-    firstName: 'NewUser2',
-    lastName: "NewSurnameUser2",
-    email: 'newuser2@gmail.com',
-    role: 'USER',
+    firstName: 'NewUser3',
+    lastName: "NewSurnameUser3",
+    email: 'newuser3@gmail.com',
+    role: 'ADMIN',
 });
 
